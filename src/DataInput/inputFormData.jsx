128985800575abbbc2c25491973343482835064f
Matt Waelder <mattwaelder@gmail.com>
2022-11-22 17:03:22 -0800

    working on form appearance

--- src/DataInput/inputFormData.jsx
+++ src/DataInput/inputFormData.jsx
@@ -15,48 +15,57 @@ const InputFormData = ({
       >
         X
       </button>
-      <select
-        name="pet_select"
-        onChange={(e) => handleChange(e)}
-        id="pet_select"
-        className="form_select"
-        required
-      >
-        <option value="">--select--</option>
-        {pets[0] && <option value="1">{pets[0]}</option>}
-        {pets[1] && <option value="2">{pets[1]}</option>}
-        {pets[2] && <option value="3">{pets[2]}</option>}
-        {pets[3] && <option value="4">{pets[3]}</option>}
-        {pets[4] && <option value="5">{pets[4]}</option>}
-      </select>
 
-      <input
-        type="date"
-        id="weigh_date"
-        onChange={(e) => handleChange(e)}
-      ></input>
+      <div className="form_sub_container">
+        <label for="pet_select">Select Pet</label>
+        <select
+          name="pet_select"
+          onChange={(e) => handleChange(e)}
+          id="pet_select"
+          className="form_select"
+          required
+        >
+          <option value="">--select--</option>
+          {pets[0] && <option value="1">{pets[0]}</option>}
+          {pets[1] && <option value="2">{pets[1]}</option>}
+          {pets[2] && <option value="3">{pets[2]}</option>}
+          {pets[3] && <option value="4">{pets[3]}</option>}
+          {pets[4] && <option value="5">{pets[4]}</option>}
+        </select>
+      </div>
 
-      <input
-        type="text"
-        id="weight"
-        className="formtext"
-        placeholder="weight"
-        onChange={(e) => handleChange(e)}
-        autocomplete="off"
-        required
-      />
+      <div className="form_sub_container">
+        <label for="weigh_date">When?</label>
+        <input
+          type="date"
+          id="weigh_date"
+          onChange={(e) => handleChange(e)}
+        ></input>
+      </div>
 
-      <select
-        name="unit"
-        onChange={(e) => handleChange(e)}
-        id="unit"
-        className="form_select"
-        required
-      >
-        <option value="">--select--</option>
-        <option value="g">g</option>
-        <option value="lbs">lbs</option>
-      </select>
+      <div className="form_sub_container">
+        <input
+          type="text"
+          id="weight"
+          className="formtext"
+          placeholder="weight"
+          onChange={(e) => handleChange(e)}
+          autocomplete="off"
+          required
+        />
+
+        <select
+          name="unit"
+          onChange={(e) => handleChange(e)}
+          id="unit"
+          className="form_select"
+          required
+        >
+          <option value="">--select--</option>
+          <option value="g">g</option>
+          <option value="lbs">lbs</option>
+        </select>
+      </div>
 
       <button
         className="form_btn"
