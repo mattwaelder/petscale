875a6c8fd15fb2e54cc6e4a4a0528c5c27ab36ea
Matt Waelder <mattwaelder@gmail.com>
2023-01-20 17:05:59 -0800

    added state for id of clicked element added bootstrap modal for confirmation of action

--- src/DataList/ListItem/index.jsx
+++ src/DataList/ListItem/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./ListItem.css";
 import utils from "../../utilities.js";
 import { please } from "../../please.js";
@@ -6,14 +7,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
-const ListItem = ({ data, user, fetchData, refresh }) => {
+const ListItem = ({ data, user, fetchData, refresh, setDelId }) => {
   const handleDel = (e) => {
-    let id = e.target.closest(".trash").id;
-    axios
-      .delete(`${utils.API}/entries/?entry=${id}`)
-      .then(() => please.fetchDataByUser(user))
-      .then(() => refresh((val) => !val))
-      .catch((err) => console.log(err));
+    // let id = e.target.closest(".trash").id;
+    //   let id = e.target.closest(".trash").id;
+    //   axios
+    //     .delete(`${utils.API}/entries/?entry=${id}`)
+    //     .then(() => please.fetchDataByUser(user))
+    //     .then(() => refresh((val) => !val))
+    //     .catch((err) => console.log(err));
   };
 
   return (
@@ -23,7 +25,7 @@ const ListItem = ({ data, user, fetchData, refresh }) => {
       </div>
       <div id="card_weight">
         <span id="weight_val">{data.weight}</span>{" "}
-        <span id="card_unit">grams</span>
+        <span id="card_unit">{(data.unit = "g" ? "grams" : "lbs")}</span>
       </div>
       <div id="card_date">
         <span>{utils.getFormattedDate(data.created_at)}</span>
@@ -32,12 +34,63 @@ const ListItem = ({ data, user, fetchData, refresh }) => {
         className="trash"
         vlaue={data}
         id={data._id}
-        onClick={(e) => handleDel(e)}
+        onClick={(e) => {
+          setDelId((delId) => data._id);
+          console.log("trash click ", data._id);
+        }}
+        // onClick={(e) => console.log(data)}
+        data-bs-toggle="modal"
+        data-bs-target="#deleteEntryModal"
       >
         <FontAwesomeIcon icon={faTrash} />
       </div>
+
+      {/* delete entry modal window */}
+      {/*
+      <div
+        class="modal fade"
+        id="deleteEntryModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Delete this entry?
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">xxx</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                onClick={(e) => handleDel(e)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 };
 
 export default ListItem;
+
+//add state for modal, move handleDel to button inside of that modal
--- src/DataList/index.jsx
+++ src/DataList/index.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
+import utils from "../utilities.js";
+import { please } from "../please.js";
 import "./DataList.css";
 import ListItem from "./ListItem";
 
 const DataList = ({ data, user, fetchData, refresh }) => {
+  // const handleDel = (e) => {
+  //   console.log(e.target);
+  //   // let id = e.target.closest(".trash").id;
+  //   // console.log(`deleting ${id}`);
+  //   // axios
+  //   //   .delete(`${utils.API}/entries/?entry=${id}`)
+  //   //   .then(() => please.fetchDataByUser(user))
+  //   //   .then(() => refresh((val) => !val))
+  //   //   .catch((err) => console.log(err));
+  // };
+  let [delId, setDelId] = useState();
+
   return (
     <div className="data_list_container">
       <h2>Past Weigh-Ins</h2>
@@ -15,10 +30,56 @@ const DataList = ({ data, user, fetchData, refresh }) => {
                 user={user}
                 fetchData={fetchData}
                 refresh={refresh}
+                setDelId={setDelId}
               />
             );
           })
         : null}
+
+      {/* modal window to delete entry */}
+
+      <div
+        class="modal fade"
+        id="deleteEntryModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Delete this entry for {`${data.name}`}?
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">{`${data.weight} ${(data.unit = "g"
+              ? "grams"
+              : "lbs")} on date`}</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                onClick={() => console.log(delId)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
